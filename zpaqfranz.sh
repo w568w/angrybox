bad7151107e32799accab229164dc4980b554b75
w568w <1278297578@qq.com>
2026-02-22 16:49:38 +0800

    fix: zpaqfranz SIGV crash

--- zpaqfranz.sh
+++ zpaqfranz.sh
@@ -6,6 +6,10 @@ set -x
 cp --recursive /io/zpaqfranz /zpaqfranz
 pushd /zpaqfranz
 
+# musl libc defaults to 128KB thread stacks, which is too small for
+# compressBlock's ~111KB Compressor object. Patch to use 8MB stacks.
+sed -i 's/pthread_create(\&tid, NULL, f, arg)/pthread_attr_t attr; pthread_attr_init(\&attr); pthread_attr_setstacksize(\&attr, 8<<20); pthread_create(\&tid, \&attr, f, arg); pthread_attr_destroy(\&attr)/' zpaqfranz.cpp
+
 $CXX $CXXFLAGS $LDFLAGS $LTOFLAGS -stdlib=libc++ -static-libstdc++ -l:libc++abi.a -Dunix zpaqfranz.cpp -o zpaqfranz.out -pthread
 
 cp ./zpaqfranz.out /io/outputs/zpaqfranz
