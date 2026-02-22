666d3eecfd3efec60976da8e3c7f16b072980048
w568w <1278297578@qq.com>
2026-02-22 12:04:57 +0800

    upgrade gdu

--- gdu.sh
+++ gdu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/dundee/gdu/releases/download/v5.30.1/gdu_linux_amd64_static.tgz
+wget https://github.com/dundee/gdu/releases/download/v5.33.0/gdu_linux_amd64_static.tgz
 
 tar -xzf gdu_linux_amd64_static.tgz
 chmod +x gdu_linux_amd64_static
